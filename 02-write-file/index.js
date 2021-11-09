698e28bc86f22a2b700be7677426a5ff2d68aa76
andrei aliashkevich <aliashkevich01@gmail.com>
2021-11-09 11:08:37 +0300

    fix: add the goodbye phrase

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,6 +8,11 @@ const textPath = path.join(__dirname, 'text.txt');
 let writeableStream = fs.createWriteStream(textPath);
 const rline = readline.createInterface({input, output});
 setTimeout(()=>{rline.write('Please, input text (for ruin the process write exit or check combination CTLR+C):\n');},100);
-rline.addListener('line', (input)=>{if(input=='exit'){process.exit(0);}writeableStream.write(input + '\n');});
+rline.addListener('line', (input)=>{if(input=='exit'){exit();}writeableStream.write(input + '\n');});
 
-rline.addListener('close', () => { process.exit(0);});
+function exit(){
+    rline.write('The end');
+    process.exit(0);
+}
+
+rline.addListener('close', exit);
